a64ba83e1e3f445800799ccb12ddb8d8a4388988
Denys <suvorov2280822@gmail.com>
2025-11-15 15:00:46 +0100

    update

---
+++ iCord/ViewModels/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  iCord
+//
+//  Created by Денис on 15/11/2025.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
